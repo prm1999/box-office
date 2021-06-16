81a6a8e671f62f94c0b6357833c284ca55b98179
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-16 21:52:28 +0530

    search box

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
 import MainPageLayouts from '../components/MainPageLayouts'
 
 const Home = () => {
+
+  const [input,setInput]=useState(' ');
+
+  
+
+  const onSearch=()=>{
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+      console.log(result);
+    })
+  };
+  const onInputChange=(ev)=>{
+    setInput(ev.target.value);
+
+  };
+  const onKeyDown=(ev)=>{
+    if (ev.KeyCode === 13){
+  onSearch();
+    }
+    console.log(ev.keyCode)
+  };
+
   return (
     <MainPageLayouts>
-      This is Home
+      <input type="text" onChange= {onInputChange}  onKeyDown={onKeyDown} value={input}/>
+      <button type='button' onClick={onSearch}> Search</button>
     </MainPageLayouts>
   )
 }
