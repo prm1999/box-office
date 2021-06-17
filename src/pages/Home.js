31bd4cd231cc9ea53b34d5b8327e9738748bf588
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-18 00:03:01 +0530

    api data fetch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React,{useState} from 'react'
 import { apiGet } from '../misc/config';
 
 import MainPageLayouts from '../components/MainPageLayouts'
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid'
+
 
 const Home = () => {
 
@@ -49,12 +52,12 @@ console.log(searchOption);
       return <div> No Results</div>
     }
     if(results && results.length>0){
-      return results[0].show? 
-      results.map(item=>(
-      <div key={item.show.id}>{ item.show.name}</div>))
-      
-      :results.map(item=>(
-        <div key={item.person.id}>{ item.person.name}</div>))
+      return results[0].show? (<ShowGrid data ={results}/>)
+      // results.map(item=>(
+      // <div key={item.show.id}>{ item.show.name}</div>))
+      :(<ActorGrid data={results}/>)
+      // results.map(item=>(
+      //   <div key={item.person.id}>{ item.person.name}</div>))
       
       
     
