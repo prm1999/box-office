09a64e760d62413c3217c75bb6156235eee37121
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-18 18:13:08 +0530

    styled

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ActorCard from './ActorCard'
 import IMAGE_NOT_FOUND  from '../../images/not-found.png'
+import {FlexGrid} from '../styled'
 
 
 
 const ActorGrid = ({data}) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(({person})=>(
         <ActorCard
          key={person.id}
@@ -19,7 +20,7 @@ const ActorGrid = ({data}) => {
         />
       ))}
       
-    </div>
+    </FlexGrid>
   )
 }
 
