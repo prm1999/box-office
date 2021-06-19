a1d56a4e9452f854226c558747ccbbcc03c2a894
prm1999 <mourya.18bcs1117@abes.ac.in>
2021-06-19 12:29:21 +0530

    reducer on show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,18 +1,51 @@
-import React ,{useEffect,useState} from 'react'
+import React ,{useEffect,useReducer} from 'react'
 import {useParams} from 'react-router-dom'
 import { apiGet } from '../misc/config';
 //  {useEffect}
+
+
+const reducer=(prevState,action)=>{
+
+  switch (action.type){
+    case 'FETCH_SUCCESS':
+      {
+        return {...prevState,isLoading:false,error:null,  show:action.show}
+      }
+
+    case 'FETCH_FAIL':{
+      return {...prevState,isLoading:false,error:action.error}
+    }
+
+      default:return prevState
+  }
+}
+
+
+
+
+const initialState={
+  show:null,
+  isLoading:true,
+  error:null
+
+}
+
+
+
 const Show = () => {
 
   const {id}=useParams();
 
-  const [show,setShow]=useState(null)
-  console.log(id);
+
+   const [state,dispatch]=useReducer(reducer,initialState )
+//  {show,isLoading,error}
+  // const [show,setShow]=useState(null)
+  // console.log(id);
 
 
-  const [isLoading,setIsLoading]=useState(true);
+  // const [isLoading,setIsLoading]=useState(true);
 
-  const [error,setError]=useState(null)
+  // const [error,setError]=useState(null)
 
   useEffect(()=>{
 
@@ -24,8 +57,10 @@ const Show = () => {
       setTimeout(()=>{
 
         if(isMounted){
-          setShow(results);
-        setIsLoading(false);
+
+          dispatch({type:'FETCH_SUCCESS',show:results})
+        //   setShow(results);
+        // setIsLoading(false);
         } 
       },2000) //  for time out function
 
@@ -35,8 +70,10 @@ const Show = () => {
     })
     .catch(err=>{
       if(isMounted){
-        setError(err.message);
-        setIsLoading(false);
+        dispatch({type:'FETCH_FAIL',error:err.message})
+
+        // setError(err.message);
+        // setIsLoading(false);
       }
       
 
@@ -47,17 +84,19 @@ const Show = () => {
      }
 
   },[id])
+  console.log('state',state)
 
-  console.log('show',show)
+
+  // console.log('show',show)
 
 
-  if(isLoading){
-    return<div>Date is loaded</div>
-  }
+  // if(isLoading){
+  //   return<div>Date is loaded</div>
+  // }
 
-  if (error){
-    return <div> Error occured ;{error}</div>
-  }
+  // if (error){
+  //   return <div> Error occured ;{error}</div>
+  // }
   return (
     <div>
   This is show pages
